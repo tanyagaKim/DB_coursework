48fca58705d594eaab50043e38dfc135bcfd545a
kim <tatchuikova@gmail.com>
2021-10-15 02:18:22 +0300

    Worked programm

---
+++ Staff_database/LoginForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Staff_database
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.kryptonPanel2 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.AuthoLabel = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.loginBox);
+            this.kryptonPanel1.Controls.Add(this.buttonLogin);
+            this.kryptonPanel1.Controls.Add(this.passwordBox);
+            this.kryptonPanel1.Controls.Add(this.pictureBox2);
+            this.kryptonPanel1.Controls.Add(this.pictureBox1);
+            this.kryptonPanel1.Controls.Add(this.kryptonPanel2);
+            this.kryptonPanel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.PanelBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.TabCustom3;
+            this.kryptonPanel1.Size = new System.Drawing.Size(432, 283);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // loginBox
+            // 
+            this.loginBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginBox.Location = new System.Drawing.Point(123, 104);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(241, 26);
+            this.loginBox.TabIndex = 6;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonLogin.Location = new System.Drawing.Point(167, 219);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.OverrideDefault.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.OverrideDefault.Content.ShortText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.OverrideFocus.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.OverrideFocus.Content.ShortText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.Size = new System.Drawing.Size(91, 41);
+            this.buttonLogin.StateCommon.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StateNormal.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StateNormal.Content.ShortText.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StatePressed.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StatePressed.Content.ShortText.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StateTracking.Content.LongText.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.StateTracking.Content.ShortText.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.TabIndex = 5;
+            this.buttonLogin.Values.Text = "Войти";
+            this.buttonLogin.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordBox.Location = new System.Drawing.Point(123, 165);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(241, 26);
+            this.passwordBox.TabIndex = 4;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Staff_database.Properties.Resources.lock_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(45, 151);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Staff_database.Properties.Resources.avatar_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Controls.Add(this.AuthoLabel);
+            this.kryptonPanel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.PanelBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.HeaderDockInactive;
+            this.kryptonPanel2.Size = new System.Drawing.Size(432, 69);
+            this.kryptonPanel2.TabIndex = 0;
+            // 
+            // AuthoLabel
+            // 
+            this.AuthoLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AuthoLabel.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.TitleControl;
+            this.AuthoLabel.Location = new System.Drawing.Point(148, 23);
+            this.AuthoLabel.Name = "AuthoLabel";
+            this.AuthoLabel.Size = new System.Drawing.Size(132, 29);
+            this.AuthoLabel.StateNormal.LongText.Font = new System.Drawing.Font("MS Outlook", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.AuthoLabel.TabIndex = 0;
+            this.AuthoLabel.Values.Text = "Авторизация";
+            // 
+            // LoginForm
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 283);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            this.kryptonPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel AuthoLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox passwordBox;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton buttonLogin;
+        private System.Windows.Forms.TextBox loginBox;
+    }
+}
---
+++ Staff_database/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Staff_database
+{
+    public partial class MainForm : Form
+    {
+        SqlConnection connection = null;
+
+        public MainForm(SqlConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Сonnectoin open error");
+                return;
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [personnel_accounting].[dbo].[Fired_card]", connection);
+
+            adapter.SelectCommand = command;
+
+            try
+            {
+                adapter.Fill(new DataTable());
+            }
+            catch
+            {
+                MessageBox.Show("Error access");
+                connection.Close();
+                return;
+            }
+
+            connection.Close();
+
+            PersonalCardForm psC = new PersonalCardForm(connection);
+            psC.TopLevel = false;
+            content.Controls.Clear();
+            content.Controls.Add(psC);
+            psC.Show();
+        }
+
+        private void kryptonButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Сonnectoin open error");
+                return;
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [personnel_accounting].[dbo].[users]", connection);
+
+            adapter.SelectCommand = command;
+
+            try
+            {
+                adapter.Fill(new DataTable());
+            }
+            catch
+            {
+                MessageBox.Show("Error access");
+                connection.Close();
+                return;
+            }
+
+            connection.Close();
+
+            SettingsForm settings = new SettingsForm(connection);
+            settings.TopLevel = false;
+            content.Controls.Clear();
+            content.Controls.Add(settings);
+            settings.Show();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void kryptonButton1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                this.connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Сonnectoin open error");
+                return;
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [personnel_accounting].[dbo].[Fired_card]", connection);
+
+            adapter.SelectCommand = command;
+
+            try
+            {
+                adapter.Fill(new DataTable());
+            }
+            catch
+            {
+                MessageBox.Show("Error access");
+                connection.Close();
+                return;
+            }
+
+            connection.Close();
+
+            FiredsForm reports = new FiredsForm(connection);
+            reports.TopLevel = false;
+            content.Controls.Clear();
+            content.Controls.Add(reports);
+            reports.Show();
+        }
+
+        private void kryptonButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Сonnectoin open error");
+                return;
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [personnel_accounting].[dbo].[Accreditation]", connection);
+
+            adapter.SelectCommand = command;
+
+            try
+            {
+                adapter.Fill(new DataTable());
+            }
+            catch
+            {
+                MessageBox.Show("Error access");
+                connection.Close();
+                return;
+            }
+
+            connection.Close();
+
+            AccreditationForm reports = new AccreditationForm(connection);
+            reports.TopLevel = false;
+            content.Controls.Clear();
+            content.Controls.Add(reports);
+            reports.Show();
+        }
+    }
+}
